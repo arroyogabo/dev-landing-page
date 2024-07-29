4133573dcd8583d7e77731498b46bc0524f3bee1
arroyogabo <107004209+arroyogabo@users.noreply.github.com>
2024-07-28 22:19:54 -0600

    update-hero

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import styles from './ProjectsStyles.module.css';
+
+function Projects() {
+  return (
+    <section id = "projects" className={styles.container}>
+        <hi className="sectionTitle">Projects</hi>
+        <div className={styles.projectsContainer}></div>
+    </section>
+
+  )
+}
+
+export default Projects
